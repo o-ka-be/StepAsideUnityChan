77b91ac33c40b4e05df56b98310be72a6883e801
o-ka-be <cosmlph.notsu@gmail.com>
2025-07-13 16:16:28 +0900

    Initial commit

---
+++ Assets/MyCameraController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour
+{
+    //unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //unityちゃんとカメラの距離
+    private float difference;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //unityちゃんとカメラの位置（z座標）の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //unityちゃんの位置に合わせてカメラの位置を移動
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+    }
+}
