188bc579394bf3e3341a6c73383e61f0b680f514
o-ka-be <cosmlph.notsu@gmail.com>
2025-07-24 20:32:59 +0900

    0724
    発展課題の制作途中経過となります

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -8,6 +8,8 @@ public class ItemGenerator : MonoBehaviour
     public GameObject coinPrefab;
     //conePrefabを入れる
     public GameObject conePrefab;
+    //ObjectMake追加する
+    public GameObject ObjectMake;
     //スタート地点
     private int startPos = 80;
     //ゴール地点
@@ -20,52 +22,53 @@ public class ItemGenerator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //一定距離ごとにアイテムを生成
-        for (int i = startPos; i < goalPos; i += 15)
-        {
-            //どのアイテムを出すのかランダムに設定
-            int num = Random.Range(1, 11);
-            if (num <= 2)
-            {
-                //コーンをx軸方向に一直線に生成
-                for (float j = -1; j <= 1; j += 0.4f)
-                {
-                    GameObject cone = Instantiate(conePrefab);
-                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
-                }
-            }
 
-            else
-            {
-                //レーンごとにアイテムを生成
-                for (int j = -1; j <= 1; j++)
-                {
-                    //アイテムの種類を決める
-                    int item = Random.Range(1, 11);
-                    //アイテムを置くz座標のオフセットをランダムに設定
-                    int offsetZ = Random.Range(-5, 6);
-                    //60%コイン配置:30％車配置:10％何もなし
-                    if (1 <= item && item <= 6)
-                    {
-                        //コインを生成
-                        GameObject coin = Instantiate(coinPrefab);
-                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetZ);
-                    }
-                    else if (7 <= item && item <= 9)
-                    {
-                        //車を生成
-                        GameObject car = Instantiate(carPrefab);
-                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
-                    }
-                }
-            }
-        }
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ObjectMakeがItemGeneratorタグにぶつかったら
+        if (ObjectMake.CompareTag("ItemGenerator"))
+        { {
+                //どのアイテムを出すのかランダムに設定
+                int num = Random.Range(1, 11);
+                if (num <= 2)
+                {
+                    //コーンをx軸方向に一直線に生成
+                    for (float j = -1; j <= 1; j += 0.4f)
+                    {
+                        GameObject cone = Instantiate(conePrefab);
+                        cone.transform.position = new Vector3(4 * j, cone.transform.position.y, ObjectMake.transform.position.z);////////////iについて座標書き換えが必要
+                    }
+                }
+
+                else
+                {
+                    //レーンごとにアイテムを生成
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        //アイテムの種類を決める
+                        int item = Random.Range(1, 11);
+                        //アイテムを置くz座標のオフセットをランダムに設定
+                        int offsetZ = Random.Range(-5, 6);
+                        //60%コイン配置:30％車配置:10％何もなし
+                        if (1 <= item && item <= 6)
+                        {
+                            //コインを生成
+                            GameObject coin = Instantiate(coinPrefab);
+                            coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, ObjectMake.transform.position.z + offsetZ);
+                        }
+                        else if (7 <= item && item <= 9)
+                        {
+                            //車を生成
+                            GameObject car = Instantiate(carPrefab);
+                            car.transform.position = new Vector3(posRange * j, car.transform.position.y, ObjectMake.transform.position.z + offsetZ);
+                        }
+                    }
+                }
+            }
+
+        }
     }
 }
